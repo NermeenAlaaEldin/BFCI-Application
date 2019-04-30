c38dcc5291b6f0b623a3d3a3ef48416373e5c9a6
nerme <nermeenalaaeldin2@gmail.com>
2019-04-30 13:03:42 +0200

    System Anl

---
+++ app/src/main/java/com/example/bfci/ItemListActivity.java
@@ -0,0 +1,9 @@
+package com.example.bfci;
+
+import android.app.FragmentManager;
+
+class ItemListActivity {
+    public FragmentManager getSupportFragmentManager() {
+        return null;
+    }
+}
